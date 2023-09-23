886b4611335bf77f3bbfb882d9c77b5d3d93a1bc
Daniel <daniel.maile@uni-ulm.de>
2023-09-23 19:37:54 +0200

    feat: add log file and custom logs to console

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 LIGHT_GREEN='\033[1;32m'
 RED='\033[0;31m'
+WHITE='\033[1;37m'
+CYAN='\033[0;36m'
 
-check_apt_installed() {
-  command -v apt &>/dev/null
+check_apt_get_installed() {
+  command -v apt-get &>/dev/null
 }
 
 create_tmp_dir() {
@@ -13,24 +15,23 @@ create_tmp_dir() {
 }
 
 perform_system_upgrade() {
-  sudo apt update
-  sudo apt upgrade -y
-  sudo apt dist-upgrade -y
-  sudo apt autoremove -y
-  sudo apt clean
+  echo "Performing system upgrade..." > /dev/tty
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get dist-upgrade -y
+  sudo apt-get autoremove -y
+  sudo apt-get clean
 }
 
 install_required_tools() {
-  sudo apt install -y curl
-  sudo apt install -y wget
-  sudo apt install -y git
-  sudo apt install -y ca-certificates
-  sudo apt install -y gnupg
+  echo "Installing required tools (curl, wget, git, ca-certificates and gnupg)..." > /dev/tty
+  sudo apt-get install -y curl wget git ca-certificates gnupg
 }
 
 add_repositories() {
   # docker
   if [ "$INSTALL_DOCKER" -eq 0 ]; then
+    echo "Adding Docker repository..." > /dev/tty
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -42,21 +43,25 @@ add_repositories() {
 
   # google cloud
   if [ "$INSTALL_MINIKUBE" -eq 0 ]; then
+    echo "Adding Google Cloud repository (for Kubernetes)..." > /dev/tty
     curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
     echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
   fi
 
+  echo "Performing system update..." > /dev/tty
   sudo apt-get update
 }
 
 clone_repo() {
+  echo "Cloning post-install git repo..." > /dev/tty
   git clone "https://github.com/dm432/post-install.git" "$TEMP_DIR"
 }
 
 ask_user() {
   if [ "$INSTALL_EVERYTHING" -eq 1 ]; then
     while true; do
-      read -r -p "$1 [Y/n]: " USER_INPUT
+      echo -n "$1 [Y/n]: " > /dev/tty
+      read -r USER_INPUT < /dev/tty
       case $USER_INPUT in
       [Yy]* | "") return 0 ;;
       [Nn]*) return 1 ;;
@@ -111,22 +116,27 @@ ask_what_to_install() {
 
 install_zsh() {
   # copy zsh dotfiles
+  echo "Copying Zsh config files..." > /dev/tty
   cp -f "$TEMP_DIR/dotfiles/.p10k.zsh" "$HOME"
   cp -f "$TEMP_DIR/dotfiles/.zshrc" "$HOME"
 
   # install zsh and set to default shell
-  sudo apt install -y zsh
+  echo "Installing Zsh..." > /dev/tty
+  sudo apt-get install -y zsh
   sudo chsh -s $(which zsh)
 
   # install oh my zsh
+  echo "Installing Oh My Zsh..." > /dev/tty
   curl -L https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
   # install zsh plugins
+  echo "Installing Zsh plugins..." > /dev/tty
   git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
   # install powerlevel10k fonts
+  echo "Installing Powerlevel10k fonts..." > /dev/tty
   POWERLEVEL10K_FONT_URLS=(
     "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
     "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
@@ -143,57 +153,69 @@ install_zsh() {
   sudo fc-cache -f -v
 
   # install powerlevel10k
+  echo "Installing Powerlevel10k..." > /dev/tty
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
   echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 }
 
 print_zsh_install_information() {
-  echo -e "${RED}Make sure to apply the MesloLGS NF font to your terminal. Otherwise powerlevel10k will not be displayed properly!"
-  echo -e "${RED}In order to make zsh your default shell, you need to log out and back in again!"
+  echo -e "${RED}Make sure to apply the MesloLGS NF font to your terminal. Otherwise powerlevel10k will not be displayed properly!${WHITE}" > /dev/tty
+  echo -e "${RED}In order to make zsh your default shell, you need to log out and back in again!${WHITE}" > /dev/tty
 }
 
 apply_vim_settings() {
-  sudo apt install -y vim-gtk3
+  echo "Applying Vim settings..." > /dev/tty
+  sudo apt-get install -y vim-gtk3
   curl -L https://raw.githubusercontent.com/dm432/vim/main/install.sh | bash
 }
 
 install_selected_tools() {
   if [ "$INSTALL_THE_FUCK" -eq 0 ]; then
-    sudo apt install thefuck
+    echo "Installing The Fuck..." > /dev/tty
+    sudo apt-get install thefuck
     echo "eval \$(thefuck --alias)" >>~/.zshrc
     source ~/.zshrc
   fi
 
   if [ "$INSTALL_DOCKER" -eq 0 ]; then
+    echo "Installing Docker..." > /dev/tty
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   fi
 
   if [ "$INSTALL_MINIKUBE" -eq 0 ]; then
+    echo "Installing Minikube..." > /dev/tty
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 --output-dir "$TEMP_DIR"
     sudo install "$TEMP_DIR/minikube-linux-amd64" /usr/local/bin/minikube
   fi
 
   if [ "$INSTALL_KUBECTL" -eq 0 ]; then
+    echo "Installing Kubectl..." > /dev/tty
     sudo apt-get install -y kubectl
   fi
 
   if [ "$INSTALL_K9S" -eq 0 ]; then
+    echo "Installing K9s..." > /dev/tty
     sudo curl -sL https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz | sudo tar xzf - -C /usr/local/bin k9s
   fi
 
   if [ "$INSTALL_HTOP" -eq 0 ]; then
-    sudo apt install htop
+    echo "Installing Htop..." > /dev/tty
+    sudo apt-get install htop
   fi
 
   if [ "$INSTALL_VIVALDI" -eq 0 ]; then
+    echo "Installing Vivaldi..." > /dev/tty
     curl -L https://downloads.vivaldi.com/snapshot/install-vivaldi.sh -o "$TEMP_DIR/install-vivaldi.sh"
     bash "$TEMP_DIR/install-vivaldi.sh" --no-launch
   fi
 }
 
 main() {
-  if ! check_apt_installed; then
-    echo >&2 "Error: apt is not installed. Aborting."
+  exec > "install.log" 2>&1
+  echo -e "The output of this script will be logged to ${CYAN}$(pwd)/install.log${WHITE}" > /dev/tty
+
+  if ! check_apt_get_installed; then
+    echo >&2 "Error: apt-get is not installed. Aborting."
     exit 1
   fi
 
@@ -214,7 +236,7 @@ main() {
 
   install_selected_tools
 
-  echo -e "${LIGHT_GREEN}All set!"
+  echo -e "${LIGHT_GREEN}All set!${WHITE}" > /dev/tty
   if [ "$INSTALL_ZSH" -eq 0 ]; then
     print_zsh_install_information
   fi
